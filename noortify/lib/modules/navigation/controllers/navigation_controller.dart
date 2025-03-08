e299be6434a51c20eb5aa94e7ae8d94a7cf0dd6d
Muhammad Hassan Raza <147485527+hassandev03@users.noreply.github.com>
2025-03-09 00:44:49 +0500

    Delete noortify directory

--- noortify/lib/modules/navigation/controllers/navigation_controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:get/get.dart';
-
-class NavigationController extends GetxController{
-  final RxInt currentIndex = 0.obs;
-
-  set changeIndex(int index) => currentIndex.value = index;
-}
